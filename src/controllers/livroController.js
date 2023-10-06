fb3c548ad5f2a6b5facf2408b6e65a6e23e7cd72
Linda Brandão <linda.15brandao@gmail.com>
2023-10-06 08:50:25 -0400

    criando o controller DELETE e sua rota

--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -51,6 +51,20 @@ class LivroController {
             })
         }
     }
+
+    static async deletarLivro (req, res) {
+        try {
+            const id = req.params.id;
+            await livro.findByIdAndDelete(id, req.body);
+            res.status(200).json({
+                message: "livro deletado!"
+            });
+        } catch (erro) {
+            res.status(500).json({
+                message: `${erro.message} - falha na exclusão do livro`
+            })
+        }
+    }    
 };
 
 export default LivroController;
