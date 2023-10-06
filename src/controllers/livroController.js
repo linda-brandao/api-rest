7f36076d71f7c4013b5be08b572f3eee4000bf75
Linda Brandão <linda.15brandao@gmail.com>
2023-10-06 08:40:39 -0400

    criando controller PUT para atualizar livro e adicionando rotas do post e do put

--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -37,6 +37,20 @@ class LivroController {
             });
         }
     }
+
+    static async atulizarLivro (req, res) {
+        try {
+            const id = req.params.id;
+            await livro.findByIdAndUpdate(id, req.body);
+            res.status(200).json({
+                message: "livro atualizado!"
+            });
+        } catch (erro) {
+            res.status(500).json({
+                message: `${erro.message} - falha na atualização do livro`
+            })
+        }
+    }
 };
 
 export default LivroController;
--- src/routes/livrosRoutes.js
+++ src/routes/livrosRoutes.js
@@ -5,5 +5,7 @@ const routes = express.Router();
 
 routes.get("/livros", LivroController.listarLivros);
 routes.get("/livros/:id", LivroController.listarLivroPorId);
+routes.post("/livros", LivroController.cadastrarLivro);
+routes.put("/livros/:id", LivroController.atulizarLivro);
 
 export default routes;
